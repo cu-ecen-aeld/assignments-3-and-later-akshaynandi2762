de352a2ae2784831650015a5bff33b9dc8adc6d1
akshaynandi2762 <gd2762@wayne.edu>
2026-01-14 10:00:02 +0530

    Commit for assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -57,7 +57,7 @@ do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./finder.sh "/tmp/assignment4-result.txt" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
